67f92dcd29ebbfb6817e6be47f8df4f407e8f3b4
sagiya <ohirakoten@i.softbank.jp>
2019-06-17 14:09:23 +0000

    add sample.php

--- sample.php
+++ sample.php
@@ -1,6 +1,3 @@
-<?php
-    $result = '無し';
-?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -9,7 +6,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <p>おみくじの結果：無し</p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
